e9da736b0f8bba46dab52be56e13a95a0fc6db2e
TFMvdBroek <tfmvdbroek@gmail.com>
2019-07-16 08:58:00 +0200

    Update Mapping.ps1

--- Mapping.ps1
+++ Mapping.ps1
@@ -14,6 +14,10 @@ $driveMappingConfig=@()
 
 $dnsDomainName= "ad.syncom.nl"
 
+$Password = "01000000d08c9ddf0115d1118c7a00c04fc297eb01000000b2537f294aacaa469f151a8132bdce8b00000000020000000000106600000001000020000000d9c406bb9e107174f834bdc4ebe5720efc1c6966b7cfe074d40377552b3ac1ba000000000e80000000020000200000009a124f0cfaededa38c5216b23f62bc20bf8b2df4d7fae038d80c190d041315c010000000bcc45f14c856bbf27f7d240d8c99ad3240000000a8e2fd5e01fe0125c8bc84956c8ad813688779f10454e7988ec85b99ec7ec16582a3d3d8e58346cc3f1c821a00220792d1ca614aa5fad4149632576210bb7eac"
+$Secure = ConvertTo-SecureString -String $Password
+$Cred = New-Object System.Management.Automation.PsCredential('analyse',$Secure)
+
 $driveMappingConfig+= [PSCUSTOMOBJECT]@{
     DriveLetter = "Z"
     UNCPath= "\\synsvfs.ad.syncom.nl\fs\Migration"
@@ -50,14 +54,6 @@ $driveMappingConfig+= [PSCUSTOMOBJECT]@{
     Description="Microscope"
 }
 
-$driveMappingConfig+= [PSCUSTOMOBJECT]@{
-    DriveLetter = "G"
-    UNCPath= "\\synbafs001.ad.syncom.nl\Data"
-    Description="Data"
-}
-
-
-
 ######################################################################
 #               end section script configuration                     #
 ######################################################################
@@ -89,14 +85,19 @@ do {
  
 }while( -not ($Connected))
 
-#Map drives
+#Map Syncom drives
     $driveMappingConfig.GetEnumerator() | ForEach-Object {
 
         Write-Output "Mapping network drive $($PSItem.UNCPath)"
 
-        New-PSDrive -PSProvider FileSystem -Name $PSItem.DriveLetter -Root $PSItem.UNCPath -Description $PSItem.Description -Persist -Scope global
+        New-PSDrive -PSProvider FileSystem -Name $PSItem.DriveLetter -Root $PSItem.UNCPath -Credential $cred -Description $PSItem.Description -Persist -Scope global
 
         (New-Object -ComObject Shell.Application).NameSpace("$($PSItem.DriveLetter):").Self.Name=$PSItem.Description
 }
+
+#Map G drive
+        New-PSDrive -PSProvider FileSystem -Name "G" -Root "\\synbafs001.ad.syncom.nl\Data" -Description "Data" -Persist -Scope global
+
+
 
 Stop-Transcript
